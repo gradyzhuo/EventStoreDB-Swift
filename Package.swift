56e596c31f737ea77717505464960c4c8bb5666f
Grady Zhuo <grady@ospark.org>
2024-01-15 01:32:24 +0800

    UPDATE: - add package `AnyCodable`.

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "eventstoredb-swift",
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +18,11 @@ let package = Package(
     dependencies: [
       .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.15.0"),
       .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-      .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+      .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+      .package(
+              url: "https://github.com/Flight-School/AnyCodable",
+              from: "0.6.0"
+          )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -24,6 +31,7 @@ let package = Package(
             name: "EventStoreDB",
             dependencies: [
                 .product(name: "Logging", package: "swift-log"),
+                .product(name: "AnyCodable", package: "AnyCodable"),
                 "GRPCSupport"
             ]),
         .target(
@@ -37,12 +45,15 @@ let package = Package(
                 .plugin(name: "SwiftProtobufPlugin", package: "swift-protobuf")
             ]),
         .executableTarget(name: "GRPCTesting", dependencies: [
-            .target(name: "EventStoreDB")
+            .target(name: "EventStoreDB"),
             ]),
         .testTarget(
             name: "EventStoreDBTests",
             dependencies: ["EventStoreDB"],
-            resources: [.copy("Resources/ca.crt")]
+            resources: [
+                .copy("Resources/ca.crt"),
+                .copy("Resources/multiple-events.json")
+            ]
             ),
     ]
 )
