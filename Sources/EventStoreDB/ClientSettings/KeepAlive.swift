fe959e0ceab78102cac2c342c569e2626313130e
Grady Zhuo <grady@ospark.org>
2024-05-28 09:36:08 +0800

    Modified file folder.

--- Sources/EventStoreDB/ClientSettings/KeepAlive.swift
+++ Sources/EventStoreDB/ClientSettings/KeepAlive.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 
-public struct KeepAlive {
-    public static var `default`: Self = .init(interval: 10.0, timeout: 10.0)
+
+public struct KeepAlive: @unchecked Sendable {
+    public static let `default`: Self = .init(interval: 10.0, timeout: 10.0)
 
     var interval: TimeInterval
     var timeout: TimeInterval
